7249afb8ed86690f9ab7ce0b4bdd604f2c41c291
Simon Brazell <simon.brazell@gmail.com>
2019-10-07 22:59:31 +1100

    Complete options & popup menus

--- background.js
+++ background.js
@@ -1,19 +1,56 @@
-const nitter = "https://nitter.net";
-const invidious = "https://invidio.us";
-const youtubeRegex = /((www|m)\.)?youtube(-nocookie)?\.com/
+'use strict';
+
+const nitterDefault = 'https://nitter.net';
+const invidiousDefault = 'https://invidio.us';
+const youtubeRegex = /((www|m)\.)?youtube(-nocookie)?\.com/;
+const pathRegex = /^https?:\/\/[^\/]+([\S\s]*)/;
+
+let nitterInstance;
+let invidiousInstance;
+let disableNitter;
+let disableInvidious;
+
+chrome.storage.sync.get(
+  ['disableNitter', 'disableInvidious', 'nitterInstance', 'invidiousInstance'],
+  (result) => {
+    disableNitter = result.disableNitter;
+    disableInvidious = result.disableInvidious;
+    nitterInstance = result.nitterInstance || nitterDefault;
+    invidiousInstance = result.invidiousInstance || invidiousDefault;
+  }
+);
+
+chrome.storage.onChanged.addListener(function (changes) {
+  if ('nitterInstance' in changes) {
+    nitterInstance = changes.nitterInstance.newValue || nitterDefault;
+  }
+  if ('invidiousInstance' in changes) {
+    invidiousInstance = changes.invidiousInstance.newValue || invidiousDefault;
+  }
+  if ('disableNitter' in changes) {
+    disableNitter = changes.disableNitter.newValue;
+  }
+  if ('disableInvidious' in changes) {
+    disableInvidious = changes.disableInvidious.newValue;
+  }
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     if (details.url.match(youtubeRegex)) {
-      return {
-        redirectUrl:
-          invidious + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]
-      };
+      if (!disableInvidious) {
+        return {
+          redirectUrl:
+            invidiousInstance + details.url.match(pathRegex)[1]
+        };
+      }
     } else {
-      return {
-        redirectUrl:
-          nitter + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]
-      };
+      if (!disableNitter) {
+        return {
+          redirectUrl:
+            nitterInstance + details.url.match(pathRegex)[1]
+        };
+      }
     }
   },
   {
--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -1,4 +1,25 @@
-document.querySelector('#options').addEventListener('click', function () {
+'use strict';
+
+let disableNitter = document.querySelector('#disableNitter');
+let disableInvidious = document.querySelector('#disableInvidious');
+
+chrome.storage.sync.get(
+  ['disableNitter', 'disableInvidious'],
+  (result) => {
+    disableNitter.checked = !result.disableNitter;
+    disableInvidious.checked = !result.disableInvidious;
+  }
+);
+
+disableNitter.addEventListener('change', (event) => {
+  chrome.storage.sync.set({ disableNitter: !event.target.checked });
+});
+
+disableInvidious.addEventListener('change', (event) => {
+  chrome.storage.sync.set({ disableInvidious: !event.target.checked });
+});
+
+document.querySelector('#options').addEventListener('click', () => {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
