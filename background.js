8e3602145d7d8886215925725c3e31b18e654176
Soheb <somoso@users.noreply.github.com>
2021-01-05 15:51:28 +0000

    Added more instances of teddit and libreddit

--- background.js
+++ background.js
@@ -110,9 +110,17 @@ const redditDomains = [
 ];
 const redditBypassPaths = /\/(gallery\/poll\/rpan\/settings\/topics)/;
 const oldRedditViews = [
-  "https://teddit.net", // privacy w/ old UI
+  // teddit: privacy w/ old UI
+  "https://teddit.net", 
+  "https://teddit.ggc-project.de",
+  "https://teddit.kavin.rocks",
   "https://snew.notabug.io", // anti-censorship
-  "https://libredd.it", // privacy w/ modern UI
+  // libreddit: privacy w/ modern UI
+  "https://libredd.it", 
+  "https://libreddit.spike.codes",
+  "https://libreddit.kavin.rocks",
+  "https://libreddit.insanity.wtf",
+  "https://libreddit.dothq.co",
   "https://old.reddit.com", // desktop
   "https://i.reddit.com" // mobile
 ];
