4565c23f19a307ddce4a6e0e4449b49d6cbc21f7
alefvanoon <alefvanoon@tuta.io>
2021-04-10 15:05:01 +0430

    add bird.trom.tf

--- src/assets/javascripts/helpers/twitter.js
+++ src/assets/javascripts/helpers/twitter.js
@@ -29,6 +29,7 @@ const redirects = [
   "https://nitter.weaponizedhumiliation.com",
   "https://nitter.vxempire.xyz",
   "https://nitter.unixfox.eu",
+  "https://bird.trom.tf",
   "http://3nzoldnxplag42gqjs23xvghtzf6t6yzssrtytnntc6ppc7xxuoneoad.onion",
   "http://nitter.l4qlywnpwqsluw65ts7md3khrivpirse744un3x7mlskqauz5pyuzgqd.onion",
   "http://nitterlgj3n5fgwesu3vxc5h67ruku33nqaoeoocae2mvlzhsu6k7fqd.onion",
