5600fdb9a33e34a0a008c22188b146e5951bf2fa
rimthekid <farbod@nigge.rs>
2021-12-07 15:09:27 +0400

    new google translate instances

--- src/assets/javascripts/helpers/google-translate.js
+++ src/assets/javascripts/helpers/google-translate.js
@@ -1,6 +1,10 @@
 const targets = ["translate.google.com", "translate.google.no"];
 
-const redirects = ["https://translate.metalune.xyz"];
+const redirects = ["https://translate.metalune.xyz",  "https://simplytranslate.org",
+"https://st.alefvanoon.xyz",
+"https://translate.josias.dev",
+"https://translate.namazso.eu",
+"https://translate.riverside.rocks",];
 
 export default {
   targets,
