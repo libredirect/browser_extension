344c2865d220e1ef77cc81bc91ff2e246799606f
ManeraKai <manerakai@protonmail.com>
2022-02-24 23:07:15 +0300

    Refining and fixing typos #30

--- src/assets/javascripts/helpers/youtubeMusic.js
+++ src/assets/javascripts/helpers/youtubeMusic.js
@@ -69,11 +69,25 @@ function redirect(url, type) {
 
     // Playlist
     // https://music.youtube.com/playlist?list=PLqxd0OMLeWy64zlwhjouj92ISc38FbOns
+    // https://music.youtube.com/playlist?list=PLqxd0OMLeWy7lrJSzt9LnOJjbC1IaruPM
+    // https://music.youtube.com/playlist?list=PLQod4DlD72ZMJmOrSNbmEmK_iZ1oXPzKd
     // https://beatbump.ml/playlist/VLPLqxd0OMLeWy64zlwhjouj92ISc38FbOns
-
+    
     // Channel
     // https://music.youtube.com/channel/UCfgmMDI7T5tOQqjnOBRe_wg
     // https://beatbump.ml/artist/UCfgmMDI7T5tOQqjnOBRe_wg
+    
+    // Albums
+    // https://music.youtube.com/playlist?list=OLAK5uy_n-9HVh3cryV2gREZM9Sc0JwEKYjjfi0dU
+    // https://music.youtube.com/playlist?list=OLAK5uy_lcr5O1zS8f6WIFI_yxqVp2RK9Dyy2bbw0
+    // https://beatbump.ml/release?id=MPREb_3DURc4yEUtD
+    // https://beatbump.ml/release?id=MPREb_evaZrV1WNdS
+
+    // https://music.youtube.com/playlist?list=OLAK5uy_n6OHVllUZUCnlIY1m-gUaH8uqkN3Y-Ca8
+    // https://music.youtube.com/playlist?list=OLAK5uy_nBOTxAc3_RGB82-Z54jdARGxGaCYlpngY
+    // https://beatbump.ml/release?id=MPREb_QygdC0wEoLe
+    
+    // https://music.youtube.com/watch?v=R6gSMSYKhKU&list=OLAK5uy_n-9HVh3cryV2gREZM9Sc0JwEKYjjfi0dU
 
     let instancesList = [...beatbumpNormalRedirectsChecks, ...beatbumpNormalCustomRedirects];
     if (instancesList.length === 0) return null;
