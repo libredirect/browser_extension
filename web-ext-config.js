77e63148c354939f8f446e858d905b5f50cbf5a6
ManeraKai <manerakai@protonmail.com>
2022-03-10 14:59:01 +0300

    Refined some things. Added an info for the Default Search Engine #101

--- web-ext-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ignoreFiles: [
-    "assets/images/Screen Shot*.png",
-    "assets/images/buy-me-a-coffee.png",
-    "assets/images/logo*.png",
-    "assets/images/badge*.png",
-  ],
-};
