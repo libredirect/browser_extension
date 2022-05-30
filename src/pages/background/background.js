854ef6115cf662c66b28d4ea2fae42882f482f8d
ManeraKai <manerakai@protonmail.com>
2022-05-31 01:54:25 +0300

    Small refining #292

--- src/pages/background/background.js
+++ src/pages/background/background.js
@@ -25,7 +25,7 @@ browser.runtime.onInstalled.addListener(
   async details => {
     // if (details.reason == 'install') {
     if (details.reason == 'install' || details.reason == "update") {
-      browser.tabs.create({ url: browser.extension.getURL("/pages/background/reset_warning.html") });
+      if (details.reason == "update") browser.tabs.create({ url: browser.extension.getURL("/pages/background/reset_warning.html") });
       fetch('/instances/blocklist.json').then(response => response.text()).then(async data => {
         await browser.storage.local.clear();
         await browser.storage.local.set({ cloudflareList: JSON.parse(data) })
