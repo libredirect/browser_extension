d17377e6a54ca4490c14d6ad2b617fd9311f2efb
ManeraKai <manerakai@protonmail.com>
2022-12-10 12:44:40 +0300

    Reverting back to Github + other stuff. Correcting http => https https://github.com/libredirect/libredirect/pull/482

--- src/pages/options/widgets/general.js
+++ src/pages/options/widgets/general.js
@@ -168,7 +168,7 @@ browser.storage.local.get("options", r => {
 				x => `<div>
                       ${x}
                       <button class="add" id="clear-${x}">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px"
+                        <svg xmlns="https://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px"
                         fill="currentColor">
                           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
                         </svg>
