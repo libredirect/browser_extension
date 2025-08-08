0bf18b7fbf117d845c9a626054a97674176d3a3c
Fijxu <fijxu@nadeko.net>
2025-08-08 19:16:14 -0400

    Add phantom

--- src/assets/javascripts/services.js
+++ src/assets/javascripts/services.js
@@ -201,6 +201,7 @@ function rewrite(url, originUrl, frontend, randomInstance, type) {
       if (query) return `${randomInstance}/search?query="${query}${mapCentre}&${utils.prefsEncoded(prefs)}`
       return `${randomInstance}/${mapCentre}&${utils.prefsEncoded(prefs)}`
     }
+    case "phantom":
     case "breezeWiki": {
       let wiki,
         urlpath = ""
@@ -955,7 +956,8 @@ const defaultInstances = {
   gocook: ["https://cook.adminforge.de"],
   wikimore: ["https://wikimore.private.coffee"],
   libreTranslate: ["https://libretranslate.com"],
-  cryptPad: ["https://cryptpad.org"]
+  cryptPad: ["https://cryptpad.org"],
+  phantom: ["https://phantom.kuuro.net"]
 }
 
 async function getDefaults() {
